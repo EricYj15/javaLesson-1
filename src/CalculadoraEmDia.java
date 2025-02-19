32ce27e4ed64c0d3a9dcdf950d5fc38675a90f33
Eric Ito <84513383+EricYj15@users.noreply.github.com>
2025-02-18 22:34:06 -0300

    Add files via upload

---
+++ src/CalculadoraEmDia.java
@@ -0,0 +1,11 @@
+public class CalculadoraEmDia {
+
+    public static void main(String[] args) {
+        int idade = 30;
+        int dias = idade * 365;
+
+        System.out.println("Voce já vivel aproximadamente " + dias + "!");
+        System.out.print("idade:" + idade + "\n" + "dias " + dias + "\n" );
+    }
+
+}
---
+++ src/ConvensorDeTemp.java
@@ -0,0 +1,10 @@
+public class ConvensorDeTemp {
+
+    public static void main(String[] args) {
+        double tempc = 36;
+        double tempf = tempc * 9 / 5 + 32;
+
+        System.out.println("Sua temperatura em Celcius é: " + tempc);
+        System.out.print("Sua temp em Fahrenheit é: " + tempf);
+    }
+}
